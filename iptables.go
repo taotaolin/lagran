f0f2699e50a9f2bce47c780c85d7a860a2132fa8
lagran007 <m@444.run>
2022-10-21 16:40:28 +0800

    优化高并发

--- iptables.go
+++ iptables.go
@@ -12,10 +12,18 @@ func SetIptable(sport string) {
 		fmt.Printf("Iptabels new error:%v", err)
 		os.Exit(1)
 	}
-	_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "SYN,ACK", "-j", "NFQUEUE", "--queue-balance", "1000:1127")
-	_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "ACK", "-j", "NFQUEUE", "--queue-balance", "2000:2127")
-	_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "PSH,ACK", "-j", "NFQUEUE", "--queue-balance", "3000:3127")
-	_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "FIN,ACK", "-j", "NFQUEUE", "--queue-balance", "4000:4127")
+	if SaEnable {
+		_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "SYN,ACK", "-j", "NFQUEUE", "--queue-balance", "1000:1127")
+	}
+	if AEnable {
+		_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "ACK", "-j", "NFQUEUE", "--queue-balance", "2000:2127")
+	}
+	if PaEnable {
+		_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "PSH,ACK", "-j", "NFQUEUE", "--queue-balance", "3000:3127")
+	}
+	if FaEnable {
+		_ = ipt.AppendUnique("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "FIN,ACK", "-j", "NFQUEUE", "--queue-balance", "4000:4127")
+	}
 }
 func UnsetIptable(sport string) {
 	ipt, err := iptables.New()
@@ -23,8 +31,16 @@ func UnsetIptable(sport string) {
 		fmt.Printf("Iptabels new error:%v", err)
 		os.Exit(1)
 	}
-	_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "SYN,ACK", "-j", "NFQUEUE", "--queue-balance", "1000:1127")
-	_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "ACK", "-j", "NFQUEUE", "--queue-balance", "2000:2127")
-	_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "PSH,ACK", "-j", "NFQUEUE", "--queue-balance", "3000:3127")
-	_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "FIN,ACK", "-j", "NFQUEUE", "--queue-balance", "4000:4127")
+	if SaEnable {
+		_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "SYN,ACK", "-j", "NFQUEUE", "--queue-balance", "1000:1127")
+	}
+	if AEnable {
+		_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "ACK", "-j", "NFQUEUE", "--queue-balance", "2000:2127")
+	}
+	if PaEnable {
+		_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "PSH,ACK", "-j", "NFQUEUE", "--queue-balance", "3000:3127")
+	}
+	if FaEnable {
+		_ = ipt.Delete("filter", "OUTPUT", "-p", "tcp", "-m", "multiport", "--sport", sport, "--tcp-flags", "SYN,RST,ACK,FIN,PSH", "FIN,ACK", "-j", "NFQUEUE", "--queue-balance", "4000:4127")
+	}
 }
--- main.go
+++ main.go
@@ -40,49 +40,57 @@ func main() {
 		UnsetIptable(Port)
 		SetIptable(Port)
 		var wg sync.WaitGroup
-		p1, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
-			packetHandle(i.(int))
-			wg.Done()
-		})
-		defer p1.Release()
-		// Submit tasks one by one.
-		log.Println("Starting Task p1")
-		for i := 1000; i < 1128; i++ {
-			wg.Add(1)
-			_ = p1.Invoke(int(i))
+		if SaEnable {
+			p1, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
+				packetHandle(i.(int))
+				wg.Done()
+			})
+			defer p1.Release()
+			// Submit tasks one by one.
+			log.Println("Starting Task p1")
+			for i := 1000; i < 1128; i++ {
+				wg.Add(1)
+				_ = p1.Invoke(int(i))
+			}
 		}
-		p2, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
-			packetHandle(i.(int))
-			wg.Done()
-		})
-		defer p2.Release()
-		// Submit tasks one by one.
-		log.Println("Starting Task p2")
-		for i := 2000; i < 2128; i++ {
-			wg.Add(1)
-			_ = p2.Invoke(int(i))
+		if AEnable {
+			p2, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
+				packetHandle(i.(int))
+				wg.Done()
+			})
+			defer p2.Release()
+			// Submit tasks one by one.
+			log.Println("Starting Task p2")
+			for i := 2000; i < 2128; i++ {
+				wg.Add(1)
+				_ = p2.Invoke(int(i))
+			}
 		}
-		p3, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
-			packetHandle(i.(int))
-			wg.Done()
-		})
-		defer p3.Release()
-		// Submit tasks one by one.
-		log.Println("Starting Task p3")
-		for i := 3000; i < 3128; i++ {
-			wg.Add(1)
-			_ = p3.Invoke(int(i))
+		if PaEnable {
+			p3, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
+				packetHandle(i.(int))
+				wg.Done()
+			})
+			defer p3.Release()
+			// Submit tasks one by one.
+			log.Println("Starting Task p3")
+			for i := 3000; i < 3128; i++ {
+				wg.Add(1)
+				_ = p3.Invoke(int(i))
+			}
 		}
-		p4, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
-			packetHandle(i.(int))
-			wg.Done()
-		})
-		defer p4.Release()
-		// Submit tasks one by one.
-		log.Println("Starting Task p4")
-		for i := 4000; i < 4128; i++ {
-			wg.Add(1)
-			_ = p4.Invoke(int(i))
+		if FaEnable {
+			p4, _ := ants.NewPoolWithFunc(128, func(i interface{}) {
+				packetHandle(i.(int))
+				wg.Done()
+			})
+			defer p4.Release()
+			// Submit tasks one by one.
+			log.Println("Starting Task p4")
+			for i := 4000; i < 4128; i++ {
+				wg.Add(1)
+				_ = p4.Invoke(int(i))
+			}
 		}
 		wg.Wait()
 	}
